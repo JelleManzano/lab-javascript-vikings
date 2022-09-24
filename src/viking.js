83ed294b8471d966753b8a084a9413c6c1e42065
Jelle <jmanzanohaagsma@gmail.com>
2022-09-24 13:26:37 +0200

    Started working on the bonus, need to refine (also need a break :P)

--- src/viking.js
+++ src/viking.js
@@ -48,4 +48,28 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+   constructor () {
+    this.vikingArmy = []
+    this.saxonArmy = []
+   }
+   addViking(vikingSoldier){
+    this.vikingArmy.push(vikingSoldier)
+   }
+   addSaxon(saxonSoldier){
+    this.saxonArmy.push(saxonSoldier)
+   }
+   vikingAttack(){
+    saxon.receiveDamage(this.viking.strength)
+   }
+   saxonAttack(){}
+   showStatus() {
+    if (this.saxonArmy.length === 0) {
+        return "Vikings have won the war of the century!"
+    } else if (this.vikingArmy[i] === 0){
+        return "Saxons have fought for their lives and survived another day..."
+    } else {
+        return "Vikings and Saxons are still in the thick of battle."
+    }
+   } 
+}
